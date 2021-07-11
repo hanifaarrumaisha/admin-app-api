e18807638421673b5edacd6a91e571b6339c10c3
Hanifa Arrumaisha <hanifa.arrumaisha@ecomindo.com>
2021-07-11 23:15:15 +0700

    add cors

--- src/main/java/com/adminapp/adminapp/controllers/ProductController.java
+++ src/main/java/com/adminapp/adminapp/controllers/ProductController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.adminapp.adminapp.service.ProductService;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
